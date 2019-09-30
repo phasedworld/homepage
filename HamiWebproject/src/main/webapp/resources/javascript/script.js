936b5d2be5b1fa0f904bbf00554b9e56addf8869
AsteriskIsStar <55785421+AsteriskIsStar@users.noreply.github.com>
2019-09-30 15:30:57 +0900

    Update script.js
    비번 재확인 까지 ok

--- HamiWebproject/src/main/webapp/resources/javascript/script.js
+++ HamiWebproject/src/main/webapp/resources/javascript/script.js
@@ -31,6 +31,10 @@ function inputCheck(){
 		alert('비밀번호를 입력하세요');
 		document.join.passwd.focus();
 	}
+	else if(document.join.passwd.value != document.join.passwd_check.value){
+		alert('비밀번호 재확인을 입력하세요')
+		document.join.passwd_check.select();
+	}
 	else if(document.join.email.value==""){
 		alert('이메일을 입력하세요');
 		document.join.email.focus();
@@ -44,5 +48,27 @@ function inputCheck(){
 	}else{
 	//서버로 데이터 전송(submit 버튼을 누른 효과)
 	document.join.submit();
+	}
+}
+
+
+function isSame() {
+	//alert('isSame start');
+	var passwd = document.join.passwd.value;
+	var passwd_check = document.join.passwd_ck.value;
+	if (passwd.length<8 || passwd.length > 16){
+		document.getElementById('pwd').innerHTML='비밀번호는 6글자 이상, 16글자 이하로 입력하세요';
+		document.getElementById('pwd').style.color='red';
+		document.getElementById('passwd').value=document.getElementById("passwd_ck").value='';
+		document.getElementById('same').innerHTML='';
+	}
+	if(document.getElementById('passwd').value != '' && document.getElementById('passwd_ck').value != ''){
+		if(document.getElementById('passwd').value == document.getElementById('passwd_ck').value){
+		document.getElementById('same').innerHTML='비밀번호가 일치합니다.';
+		document.getElementById('same').style.color='green';
+		}else{
+			document.getElementById('same').innerHTML='비밀번호가 일치하지 않습니다.';
+			document.getElementById('same').style.color='red';
+		}
 	}
 }
